2e2f482682b1b2621ec639cdf28339c0ae5b1598
Alec Jacobs <ajacobs@invoca.com>
2016-02-06 15:39:28 -0800

    Enable get of json files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var recipes = './lib/recipes/';
 var app = express();
 
 // view engine setup
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/recipe', express.static(recipes));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -55,6 +57,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
 
 
 module.exports = app;
