c2b9dd64a1a25b4f8d2dd0501fe8f4758fed58fc
Sharat Virunchipuram <thesharatv@gmail.com>
2016-02-06 18:35:01 -0800

    mogoose schemas and models

--- models/Country.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var CountrySchema = new mongoose.Schema({
-    name: String,
-    recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipes' }]
-});
-
-mongoose.model('Country', CountrySchema);
